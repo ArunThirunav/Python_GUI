095dee165b8c9b3155feedd5bec135dbb7077720
ArunThirunavukkarasu <arunthirunavukkarasu96@gmail.com>
2023-03-20 20:38:26 +0530

    Click button to enter function

--- Iot.py
+++ Iot.py
@@ -1,9 +1,13 @@
-from PyQt5.QtWidgets import QDialog, QPushButton, QApplication
+from PyQt5.QtWidgets import QApplication
 from PyQt5 import uic
 import sys
 
+def func():
+    print("Hello")
 
-app = QApplication(sys.argv)
-win = uic.loadUi('frontEnd.ui')
-win.show()
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    dlg = uic.loadUi('frontEnd.ui')
+    dlg.pushButton.clicked.connect(func)
+    dlg.show()
 app.exec()
